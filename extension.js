f2c36f320420ec2ebcf2becfcf9abb2e00a19fd8
Konstantin Trifonov <trifonovkv@gmail.com>
2020-12-22 13:10:18 +0500

    fix: matches on the wrong numbers of the ping output (fix #16)

--- extension.js
+++ extension.js
@@ -102,7 +102,8 @@ const PingMenuButton = new Lang.Class({
     _loadPipeOUT: function(channel, condition, data) {
         if (condition != GLib.IOCondition.HUP) {
             let [size, out] = channel.read_to_end();
-            let result = String.fromCharCode.apply(null, out).match(/\d+(?=(\.\d)?\s\w+$)/m);
+            let result = String.fromCharCode.apply(null, out).match(/(?<=\w=)\d+(?=(.\d+)?\s\w+$)/m);
+
             if(result != null) {
                 let str = result[0];
                 str = str.concat(_(" ms"));
